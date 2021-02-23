71a0a803bfd56f5c55c99778db6e2b69894fb74f
Ruslan Penkrat <penkrat@gmail.com>
2021-02-23 08:36:55 +0300

    initial commit

---
+++ src/test/java/com/github/russ_p/resliv/bot/ReslivBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.russ_p.resliv.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReslivBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
